8f823d16d14afdade64bee196013e301eeda1dec
Weyne <augustoweyne12@gmail.com>
2025-07-13 14:19:28 -0300

    improv: recarregando horarios  disponiveis

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 const form = document.querySelector("form");
 const selectedDate = document.getElementById("date");
+const clienteName = document.getElementById("client");
 
 //Data atual para o input
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
@@ -13,4 +14,39 @@ selectedDate.min = inputToday;
 
 form.onsubmit = (event) => {
     event.preventDefault();
+
+    try {
+        //Recuperando o nome do cliente
+        const name = clienteName.value.trim();
+
+        if(!name) {
+            return alert("Informe o nome do cliente!");
+        }
+
+        //Recuperar o horário selecionado.
+        const hourSelected = document.querySelector(".hour-selected");
+
+        //Verifica se tem um horário selecionado.
+        if(!hourSelected) {
+            return alert("Selecione a hora.");
+        }
+
+        // Recupera somente a hora
+        const [hour] = hourSelected.innerHTML.split(":");
+        
+        //Inserir a hora na data
+        const when = dayjs(selectedDate.value).add(hour, "hour");
+
+        //Gerar um ID
+        const id = new Date().getTime();
+
+        const objHors = {
+            id,
+            name,
+            when
+        };
+    }catch(error){
+        alert("Não foi possível realizar o agendamento.");
+        console.log(error);
+    }
 }
